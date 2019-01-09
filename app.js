d5df6f05d38cc1cbc0f39b63dddcc07b4e963485
jwesinspace <westracompsci@gmail.com>
2019-01-09 00:03:01 -0500

    Removing extra files

--- app.js
+++ app.js
@@ -2,21 +2,17 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
-// TO DO - ADD SOCKET.IO
-
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 app.post("/api/login", (req, res) => {
-    console.log("BODY IN LOGIN", req.body);
-    res.body = {"username": req.body.username};
+    res.body = { "username": req.body.username };
     res.redirect("/api/messages/" + req.body.username);
 });
 
 app.use("/api/messages/:username", (req, res) => {
-    console.log(req.params);
-    res.render("chatroom", {username: req.params.username});
+    res.render("chatroom", { username: req.params.username });
 });
 
 app.get("/", function (req, res) {
--- config.js
+++ config.js
@@ -1,3 +1,4 @@
+// TO DO - MOVE THIS TO AN ENV VAR
 module.exports = {
     'secret': 'suchsecret'
 };
