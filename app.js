41d9a294fbb68e8bfcccc4065153eb643791f0fb
jwesinspace <westracompsci@gmail.com>
2019-01-11 09:29:17 -0500

    Improving static file serving

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
+const path = require('path');
 const io = require('socket.io')(http);
 
-app.use(express.static("scripts"));
-app.use(express.static("styles"));
+const staticScriptsPath = './public/scripts';
+const staticCSSPath = './public/styles';
+
+app.use('/scripts', (req, res) => {
+    const resolvedBase = path.resolve(staticScriptsPath);
+    const safeSuffix = path.normalize(req.url).replace(/^(\.\.[\/\\]]])+/, '');
+    const fileLoc = path.join(resolvedBase, safeSuffix);
+    console.log('File Location: ' + fileLoc);
+    res.sendFile(fileLoc);
+});
+
+app.use('/styles/fonts', (req, res) => {
+    console.log("font request received");
+    res.sendFile(__dirname + '/public/styles/fonts/Rubik/Rubik-Regular.ttf');
+});
+
+app.use('/styles', (req, res) => {
+    const resolvedBase = path.resolve(staticCSSPath);
+    const safeSuffix = path.normalize(req.url).replace(/^(\.\.[\/\\]]])+/, '');
+    const fileLoc = path.join(resolvedBase, safeSuffix);
+    console.log('File Location: ' + fileLoc);
+    res.sendFile(fileLoc);
+});
 
 app.use('/', (req, res) => {
     res.sendFile(__dirname + '/chat.html');
